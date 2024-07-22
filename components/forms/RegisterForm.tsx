337b52fcb7a21e5584dc7bd2f555ef315dfdeb6f
Kavin Bharathi <r.m.kavinbharathi@gmail.com>
2024-07-22 22:27:17 +0530

    Init: Patient management system

---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,194 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import CustomFormField from "@/components/CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { Form, FormControl } from "../ui/form"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
+import { FormFieldType } from "./PatientForm"
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import { Doctors, GenderOptions } from "@/constants"
+import { Label } from "../ui/label"
+import { SelectItem } from "../ui/select"
+import Image from "next/image"
+
+const RegisterForm = ({ user }: { user: User }) => {
+
+    const router = useRouter()
+    const [isLoading, setIsLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: "",
+        },
+    })
+
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true)
+
+        try {
+            const userData = { name, email, phone }
+            const user = await createUser(userData)
+            if (user) router.push(`/patients/${user.$id}/register`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+                <section className="space-y-4">
+                    <h1 className="header">Welcome👋</h1>
+                    <p className="text-dark-700">Let us know more about yourself</p>
+                </section>
+
+                <section className="space-y-6">
+                    <div className="mb-9 space-y-1">
+                        <h2 className="sub-header">Personal Information</h2>
+                    </div>
+                </section>
+
+                <CustomFormField
+                    control={form.control}
+                    fieldType={FormFieldType.INPUT}
+                    name="name"
+                    label="Full Name"
+                    placeholder="John Doe"
+                    iconSrc="/assets/icons/user.svg"
+                    iconAlt="user"
+                />
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        control={form.control}
+                        fieldType={FormFieldType.INPUT}
+                        name="email"
+                        label="Email"
+                        placeholder="johndoe@gmail.com"
+                        iconSrc="/assets/icons/email.svg"
+                        iconAlt="email"
+                    />
+
+                    <CustomFormField
+                        control={form.control}
+                        fieldType={FormFieldType.PHONE_INPUT}
+                        name="phone"
+                        label="Phone Number"
+                        placeholder="(555) 1234567"
+                    />
+                </div>
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        control={form.control}
+                        fieldType={FormFieldType.DATE_PICKER}
+                        name="birthDate"
+                        label="Date of Birth"
+                    />
+
+                    <CustomFormField
+                        control={form.control}
+                        fieldType={FormFieldType.SKELETON}
+                        name="gender"
+                        label="Gender"
+                        renderSkeleton={(field) => (
+                            <FormControl>
+                                <RadioGroup className="flex h-11 gap-6 xl:justify-between"
+                                    onValueChange={field.onChange}
+                                    defaultValue={field.value}>
+                                    {GenderOptions.map((option) => (
+                                        <div key={option} className="radio-group">
+                                            <RadioGroupItem value={option} id={option} />
+                                            <Label htmlFor={option} className="cursor-pointer">
+                                                {option}
+                                            </Label>
+                                        </div>
+                                    ))}
+                                </RadioGroup>
+                            </FormControl>
+                        )}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        control={form.control}
+                        fieldType={FormFieldType.INPUT}
+                        name="address"
+                        label="Address"
+                        placeholder="2nd, 4th Cross, Bangalore"
+                    />
+
+                    <CustomFormField
+                        control={form.control}
+                        fieldType={FormFieldType.INPUT}
+                        name="occupation"
+                        label="Occupation"
+                        placeholder="Software Engineer"
+                    />
+                </div>
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        control={form.control}
+                        fieldType={FormFieldType.INPUT}
+                        name="emergencyContactName"
+                        label="Emergency Contact Name"
+                        placeholder="Guardian's name"
+                    />
+
+                    <CustomFormField
+                        control={form.control}
+                        fieldType={FormFieldType.PHONE_INPUT}
+                        name="emergencyContactNumber"
+                        label="Emegency Contact Number"
+                        placeholder="(555) 1234567"
+                    />
+                </div>
+
+                <section className="space-y-6">
+                    <div className="mb-9 space-y-1">
+                        <h2 className="sub-header">Medical Information</h2>
+                    </div>
+                </section>
+
+                <CustomFormField
+                    control={form.control}
+                    fieldType={FormFieldType.SELECT}
+                    name="primaryPhysician"
+                    label="Primary Physician"
+                    placeholder="Select a physician"
+                >
+
+                    {Doctors.map((doctor) => (
+                        <SelectItem key={doctor.name} value={doctor.name}>
+                            <div className="flex cursor-pointer items-center gap-2">
+                                <Image 
+                                    src={doctor.image}
+                                    height={32}
+                                    width={32}
+                                    alt={doctor.name}
+                                    className="rounded-full border border-dark-500"
+                                />
+                                <p>{doctor.name}</p>
+                            </div>
+                        </SelectItem>
+                    ))}
+                </CustomFormField>
+
+                <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+            </form>
+        </Form>
+    )
+}
+
+export default RegisterForm
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        PROJECT_ID: process.env.PROJECT_ID,
+        API_KEY: process.env.API_KEY,
+        DATABASE_ID: process.env.DATABASE_ID,
+        PATIENT_COLLECTION_ID: process.env.PATIENT_COLLECTION_ID,
+        DOCTOR_COLLECTION_ID: process.env.DOCTOR_COLLECTION_ID,
+        APPOINTMENT_COLLECTION_ID: process.env.APPOINTMENT_COLLECTION_ID,
+        BUCKET_ID: process.env.BUCKET_ID,
+        ENDPOINT: process.env.ENDPOINT
+    }
+};
 
 export default nextConfig;
